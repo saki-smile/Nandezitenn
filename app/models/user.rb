a35f7e4744ba2e26d9730379e3328a0cfc3cceeb
saki-smile <sa.su.ko0419@gmail.com>
2021-01-12 15:54:31 +0000

    [add] フォロー機能の非同期通信化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,22 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :favorite_questions, through: :favorites, source: :question
+  has_many :favorite_questions, through: :favorites, source: :question # お気に入りの投稿
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
 
 end
