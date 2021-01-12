eabe6c23e9c13d854821eff38340394b3d470ba9
saki-smile <sa.su.ko0419@gmail.com>
2021-01-12 02:55:03 +0000

    [update] コントローラーの重複部分修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,23 @@
 class FavoritesController < ApplicationController
+
+  before_action :set_question, only: [:create, :destroy]
+
   def create
-    @question = Question.find(params[:question_id])
     favorite = current_user.favorites.build(question_id: @question.id)
     favorite.save
     render :index
   end
 
   def destroy
-    @question = Question.find(params[:question_id])
     favorite = current_user.favorites.find_by(question_id: @question.id)
     favorite.destroy
     render :index
   end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :favorite, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @questions = @user.questions.page(params[:page]).reverse_order
-    @favorite_questions = @user.favorite_questions
   end
 
   def index
@@ -11,16 +12,13 @@ class UsersController < ApplicationController
   end
 
   def favorite
-    @user = User.find(params[:id])
     @favorite_questions = @user.favorite_questions.page(params[:page]).reverse_order
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
@@ -29,6 +27,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
