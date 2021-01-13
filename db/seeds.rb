0ef2b944615ceb64c747c6d47c472577b669d417
saki-smile <sa.su.ko0419@gmail.com>
2021-01-13 17:09:27 +0000

    [add]管理者権限のユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,3 +40,11 @@ Category.create!(
     },
   ]
 )
+
+User.create!(
+  name: '管理者',
+  email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD_CONFIRMATION'],
+  admin: true
+)
