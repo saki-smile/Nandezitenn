0c537013fa25e6b2b7199f51d07568b33f8259ad
saki-smile <sa.su.ko0419@gmail.com>
2021-01-13 15:30:45 +0000

    [add] ランキング機能追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,13 @@
 class HomesController < ApplicationController
+
   def top
+    @question_ranks = Question.joins(:favorites).group(:question_id).order('count(question_id) desc').limit(3)
+    #joins テーブルを内部結合
+    #group question_idが同じものにグループを分ける
+    #order question_idの多い順番に並び替える
   end
 
   def about
   end
+
 end
