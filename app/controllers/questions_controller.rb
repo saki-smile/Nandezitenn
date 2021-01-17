7e55d67637cc545f94f76acc6972580ebed421b3
saki-smile <sa.su.ko0419@gmail.com>
2021-01-17 06:12:26 +0000

    [add] 投稿詳細、検索レイアウト

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_categories, only: [:index, :show]
+  before_action :set_categories, only: [:index, :show, :search]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def search
