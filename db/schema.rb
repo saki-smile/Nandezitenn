2ad72ae8c5bb88407a939d9ab5e37045bb2b4bd8
saki-smile <sa.su.ko0419@gmail.com>
2021-01-20 03:07:48 +0000

    [fix] マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2021_01_12_032619) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.integer "user_id"
     t.integer "category_id"
