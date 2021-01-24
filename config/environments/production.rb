d324fe7360f08ce1aa43e46dcd184cbc27eef390
saki-smile <sa.su.ko0419@gmail.com>
2021-01-24 11:32:42 +0000

    [add] actionmailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,17 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV['INQUIRY_MAIL'],
+    password:             ENV['INQUIRY_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
