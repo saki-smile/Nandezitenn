fd35dc984ec42afe5954be3da3eab0f8d0a396ee
saki-smile <sa.su.ko0419@gmail.com>
2021-02-01 02:36:23 +0000

    [add] メイラーのサブジェクト修正

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -3,7 +3,6 @@ class InquiryMailer < ApplicationMailer
 
   def received_email(inquiry)
     @inquiry = inquiry
-    # mail to: ENV["INQUIRY_MAIL"], subject: '【サイト名】 お問い合わせありがとうございます'
     mail(:to => ENV["INQUIRY_MAIL"], :subject => 'お問い合わせを承りました')
   end
 end
