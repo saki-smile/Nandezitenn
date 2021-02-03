4f24adb39a5b555aa397e051f9ebb616ac250d37
saki-smile <sa.su.ko0419@gmail.com>
2021-02-03 16:36:46 +0000

    [add] コメントのバリデーション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,11 @@ class CommentsController < ApplicationController
     @comment = @question.comments.build(comment_params)
     @comment.score = Language.get_data(comment_params[:body])
     @comment.user_id = current_user.id
-    @comment.save
-    render :index
+    if @comment.save
+      render :index
+    else
+      render :error
+    end
   end
 
   def destroy
