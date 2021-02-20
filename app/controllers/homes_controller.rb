bf70700cebcc3e07e12c3b53d237d2aaea3dfce9
saki-smile <sa.su.ko0419@gmail.com>
2021-02-20 14:51:02 +0000

    add rspec-system-test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @rank = Question.joins(:favorites).group(:question_id).order('count(question_id) desc').limit(3)
+    @rank = Question.joins(:favorites).group(:question_id).order(Arel.sql('count(question_id) desc')).limit(3)
   end
 
   def about
